8d062659c903f657208fb1eccd2441b28c899476
Pratiman Shahi <spratiman@yahoo.com>
2022-06-08 08:33:34 -0400

    Add Visits api view

--- northbridge_app/northbridge/views.py
+++ northbridge_app/northbridge/views.py
@@ -1,6 +1,6 @@
 from rest_framework_json_api.views import ModelViewSet
-from northbridge.serializers import UserSerializer, PolicySerializer, PageSerializer
-from northbridge.models import User, Policy, Page
+from northbridge.serializers import UserSerializer, PolicySerializer, PageSerializer, VisitsSerializer
+from northbridge.models import User, Policy, Page, Visits
 
 
 class UserAPIView(ModelViewSet):
@@ -19,3 +19,9 @@ class PageAPIView(ModelViewSet):
     serializer_class = PageSerializer
     http_methods = ["get"]
     queryset = Page.objects.all()
+
+
+class VisitsAPIView(ModelViewSet):
+    serializer_class = VisitsSerializer
+    http_methods = ["get"]
+    queryset = Visits.objects.all()
