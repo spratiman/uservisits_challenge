16bd2fc7cd3c20610158878239293501d126ef96
Pratiman Shahi <spratiman@yahoo.com>
2022-06-08 08:32:08 -0400

    Add Policy api view

--- northbridge_app/northbridge/views.py
+++ northbridge_app/northbridge/views.py
@@ -1,9 +1,15 @@
 from rest_framework_json_api.views import ModelViewSet
-from northbridge.serializers import UserSerializer
-from northbridge.models import User
+from northbridge.serializers import UserSerializer, PolicySerializer
+from northbridge.models import User, Policy
 
 
 class UserAPIView(ModelViewSet):
     serializer_class = UserSerializer
     http_methods = ["get"]
     queryset = User.objects.all()
+
+
+class PolicyAPIView(ModelViewSet):
+    serializer_class = PolicySerializer
+    http_methods = ["get"]
+    queryset = Policy.objects.all()
