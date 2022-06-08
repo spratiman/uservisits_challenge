e7acfd6beba71db35d2aa63bf2782318d47e6583
Pratiman Shahi <spratiman@yahoo.com>
2022-06-08 08:31:28 -0400

    Add User api view

--- northbridge_app/northbridge/views.py
+++ northbridge_app/northbridge/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework_json_api.views import ModelViewSet
+from northbridge.serializers import UserSerializer
+from northbridge.models import User
 
-# Create your views here.
+
+class UserAPIView(ModelViewSet):
+    serializer_class = UserSerializer
+    http_methods = ["get"]
+    queryset = User.objects.all()
