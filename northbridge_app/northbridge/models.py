7865d0ec52210bb2426ce7c37bcaab0207c00205
Pratiman Shahi <spratiman@yahoo.com>
2022-06-08 07:48:59 -0400

    Add User model

--- northbridge_app/northbridge/models.py
+++ northbridge_app/northbridge/models.py
@@ -1,3 +1,31 @@
+from django.core import validators
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    Users onboarded to the Northbridge website
+    """
+
+    name = models.CharField(
+        max_length=30,
+        help_text=_(
+            "Required. Between 2 and 30 characters."
+            "Cannot start or end with a space."
+            "No special symbols outside of accented letters."
+        ),
+        validators=[
+            validators.RegexValidator(
+                r"^\w[\w ]*\w+$",
+                _(
+                    "Enter a valid name. At least 2 characters, "
+                    "cannot start or end with a space."
+                    "No special symbols outside of accented letters."
+                ),
+                "invalid",
+            )
+        ],
+    )
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
