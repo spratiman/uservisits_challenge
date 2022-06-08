2e5d349f7b9ec8784ac44aa44bab43e6b3a621bd
Pratiman Shahi <spratiman@yahoo.com>
2022-06-08 07:51:58 -0400

    Add Policy model

--- northbridge_app/northbridge/models.py
+++ northbridge_app/northbridge/models.py
@@ -1,3 +1,6 @@
+import os
+
+from binascii import hexlify
 from django.core import validators
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -29,3 +32,42 @@ class User(models.Model):
     )
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
+
+
+def _create_hash_id():
+    """
+    Generates a 10 character long hash
+    """
+    return hexlify(os.random(10))
+
+
+class Policy(models.Model):
+    """
+    Northbridge Policy related to each user
+    """
+
+    PENDING_POLICY = "P"
+    ACTIVE_POLICY = "A"
+    EXPIRED_POLICY = "E"
+
+    POLICY_CHOICES = (
+        (PENDING_POLICY, "Pending"),
+        (ACTIVE_POLICY, "Active"),
+        (EXPIRED_POLICY, "Expired")
+    )
+
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.DO_NOTHING,
+        related_name="user_policy",
+        blank=False,
+        null=False
+    )
+    hash_id = models.CharField(
+        max_length=10, default=_create_hash_id, unique=True)
+    start_date = models.DateTimeField()
+    state = models.CharField(
+        choices=POLICY_CHOICES,
+        default=PENDING_POLICY,
+        max_length=1,
+    )
